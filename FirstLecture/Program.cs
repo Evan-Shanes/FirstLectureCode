8178408bb1ae1e03acd8f6ade570b64a73fe886d
Evan Shanes <evan.shanes@ou.edu>
2022-08-25 15:05:58 -0500

    Add project files.

---
+++ FirstLecture/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLecture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //data type then variable name
+
+            int age=0;
+            int Age;
+
+            Age = 20 + 6;
+
+            bool isA;
+
+            double weight;
+            weight = 128.8;
+
+            char ch = 'a';
+
+            Console.Write(Age);
+
+            Console.Write(ch);
+
+            Console.Write(age);
+
+            Console.ReadLine();
+
+        //these two are different variables because of the capital letter
+
+          
+        }
+    }
+}
